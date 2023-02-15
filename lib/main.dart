ed50bb3bfaa6be0cc63172c01dd53dd1dc7f799f
2dowon <dwon424@gmail.com>
2023-02-15 18:45:35 +0900

    feat: BuildContext

--- lib/main.dart
+++ lib/main.dart
@@ -12,44 +12,43 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
-
-  void onButtonClick() {
-    setState(() {
-      counter += 1;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
           backgroundColor: const Color(0xFFF4EDDB),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Click Count",
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                Text(
-                  "$counter",
-                  style: const TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                IconButton(
-                  onPressed: onButtonClick,
-                  icon: const Icon(
-                    Icons.add_box_rounded,
-                  ),
-                )
+              children: const [
+                MyLargeTitle(),
               ],
             ),
           )),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "My Large Title",
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
+      ),
     );
   }
 }
