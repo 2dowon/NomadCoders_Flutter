f15b9bdb74f8b9d24191ed61ebf8bf68f62635b0
2dowon <dwon424@gmail.com>
2023-02-20 15:31:11 +0900

    feat: ApiServer

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
+import '../models/webtoon_detail_model.dart';
+import '../models/webtoon_episode_model.dart';
+
 class ApiService {
   static const String baseUrl =
       "https://webtoon-crawler.nomadcoders.workers.dev";
@@ -16,6 +19,31 @@ class ApiService {
       final List<dynamic> webtoons = jsonDecode(response.body);
 
       return webtoons.map((webtoon) => WebtoonModel.fromJson(webtoon)).toList();
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      return episodes
+          .map((episode) => WebtoonEpisodeModel.fromJson(episode))
+          .toList();
     }
     throw Error();
   }
